ee0c782cefe449e57c60c8cc2ac25c481a6a0dab
SANJANA KATTA <sanjanaa.katta@gmail.com>
2025-09-08 12:16:34 +0530

    Add files via upload

---
+++ MovieTicketManagement-1/MovieTicketManagement-1/src/test/java/com/example/movieticketmanagement/MovieTicketManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieticketmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
